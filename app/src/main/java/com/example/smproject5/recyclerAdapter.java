890e410d68285e34e63d5082ba919ff7410460d4
harshPatel1818 <harshpatel081801@gmail.com>
2022-04-21 18:21:28 -0400

    rv

--- app/src/main/java/com/example/smproject5/recyclerAdapter.java
+++ app/src/main/java/com/example/smproject5/recyclerAdapter.java
@@ -1,18 +1,16 @@
 package com.example.smproject5;
-import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -28,11 +26,14 @@ public class recyclerAdapter extends RecyclerView.Adapter<recyclerAdapter.MyView
     public class MyViewHolder extends RecyclerView.ViewHolder
     {
         private TextView donutName;
+        private TextView donutPrice;
+
 
         public MyViewHolder(final View view)
         {
             super(view);
-            donutName = view.findViewById(R.id.textView2);
+            donutName = view.findViewById(R.id.name);
+            donutPrice = view.findViewById(R.id.price);
 
         }
     }
@@ -46,12 +47,96 @@ public class recyclerAdapter extends RecyclerView.Adapter<recyclerAdapter.MyView
 
     @Override
     public void onBindViewHolder(@NonNull recyclerAdapter.MyViewHolder holder, int position) {
-        String n = donutList.get(position).toString();
+        String n = donutList.get(position).getFlavor()+donutList.get(position).getType();
         holder.donutName.setText(n);
+        holder.donutPrice.setText(""+donutList.get(position).getPrice());
     }
 
     @Override
     public int getItemCount() {
         return donutList.size();
     }
+
+    public static class donutHolder extends RecyclerView.ViewHolder
+    {
+        private TextView name,dprice;
+        private Button ab;
+        View rootView;
+        int position;
+        Donut donut;
+       // private ConstraintLayout parentLayout;
+        public donutHolder(@NonNull View itemView)
+        {
+         super(itemView);
+         rootView = itemView;
+         name = itemView.findViewById(R.id.name);
+         dprice = itemView.findViewById(R.id.price);
+         itemView.setOnClickListener(new View.OnClickListener(){
+
+             @Override
+             public void onClick(View view) {
+                 Log.d("demo","onCLick: item clicked "+position+"donut" +donut.getFlavor());
+             }
+         });
+
+         itemView.findViewById(R.id.addButton).setOnClickListener(new View.OnClickListener(){
+
+             @Override
+             public void onClick(View view) {
+                 Log.d("demo","onCLick: item clicked "+donut.getFlavor());
+                 System.out.println(position);
+             }
+         });
+
+        }
+
+        /*
+
+        super(itemView);
+            name = itemView.findViewById(R.id.name);
+            dprice = itemView.findViewById(R.id.price);
+           // im_item = itemView.findViewById(R.id.im_item);
+            ab = itemView.findViewById(R.id.addButton);
+            parentLayout = itemView.findViewById(R.id.rowLayout);
+            setAddButtonOnClick(itemView); //register the onClicklistener for the button on each row.
+
+            parentLayout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(itemView.getContext(), ItemSelectedActivity.class);
+                    intent.putExtra("ITEM", name.getText());
+                    itemView.getContext().startActivity(intent);
+                }
+            });
+
+
+
+        private void setAddButtonOnClick(@NonNull View itemView) {
+            ab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    AlertDialog.Builder alert = new AlertDialog.Builder(itemView.getContext());
+                    alert.setTitle("Add to order");
+                    alert.setMessage(name.getText().toString());
+                    //handle the "YES" click
+                    alert.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Toast.makeText(itemView.getContext(),
+                                    name.getText().toString() + " added.", Toast.LENGTH_LONG).show();
+                        }
+                        //handle the "NO" click
+                    }).setNegativeButton("no", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Toast.makeText(itemView.getContext(),
+                                    name.getText().toString() + " not added.", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                    AlertDialog dialog = alert.create();
+                    dialog.show();
+                }
+            });
+        }
+        */
+    }
+
 }
